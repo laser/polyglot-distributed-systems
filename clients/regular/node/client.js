ecfaca9b319ed79ffd34815bec2fbc57a835685c
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-01 14:03:55 -0700

    adding PHP and Node.js basic client examples

---
+++ clients/regular/node/client.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var barrister = require('barrister');
+var client    = barrister.httpClient('http://localhost:3000/todos');
+
+client.loadContract(function(err) {
+  var proxy      = client.proxy('TodoManager');
+  var properties = { 'title' : 'Call Dad', 'completed' : false };
+
+  proxy.createTodo(properties, function(err, result) {
+    if (err) {
+      console.log(JSON.stringify(err));
+    }
+    else {
+      console.log(JSON.stringify(result));
+    }
+  });
+});
---
+++ clients/regular/php/client.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+
+<?php
+
+include_once("./lib/barrister.php");
+
+$barrister = new Barrister();
+$client    = $barrister->httpClient("http://localhost:3000/todos");
+$proxy     = $client->proxy("TodoManager");
+
+try {
+  $result = $proxy->createTodo([ "title" => "Call Mom", "completed" => false ]);
+  var_dump($result);
+}
+catch (BarristerRpcException $e) {
+  echo "err.code=" . $e->getCode() . "\n";
+}
+
+?>
