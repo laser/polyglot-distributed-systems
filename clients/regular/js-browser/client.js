20339bb71ae469dbf4ec03c7055401edf99f5b6b
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-01 14:13:26 -0700

    browser-based JS example

---
+++ clients/regular/js-browser/client.js
@@ -0,0 +1,15 @@
+var client = Barrister.httpClient("/todos");
+
+client.loadContract(function() {
+  var proxy      = client.proxy('TodoManager');
+  var properties = { 'title' : 'Call Dad', 'completed' : false };
+
+  proxy.createTodo(properties, function(err, result) {
+    if (err) {
+      console.log(JSON.stringify(err));
+    }
+    else {
+      console.log(JSON.stringify(result));
+    }
+  });
+});
