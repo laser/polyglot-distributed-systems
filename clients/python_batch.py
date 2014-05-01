bf24db989af689e4a573e2de5dcd9df69aaaf40a
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-01 10:04:51 -0700

    Python batch client example

---
+++ clients/python_batch.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import barrister
+
+trans  = barrister.HttpTransport("http://localhost:3000/todos")
+client = barrister.Client(trans)
+
+batch = client.start_batch()
+batch.TodoManager.createTodo({ "title" : "Call Mom", "completed" : False })
+batch.TodoManager.createTodo({ "title" : "Call Dad", "completed" : False })
+batch.TodoManager.createTodo({ "title" : "Wash car", "completed" : False })
+batch.TodoManager.createTodo({ "title" : "Eat Ham", "completed" : False })
+
+results = batch.send()
+for res in results:
+    # either res.error or res.result will be set
+    if res.error:
+        # res.error is a barrister.RpcException, so you can raise it if desired
+        print "err.code=%d" % res.error.code
+    else:
+        # result from a successful call
+        print res.result
