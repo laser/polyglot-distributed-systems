32ca2d3be502a2419a604d4d7ca8105fded5161c
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-01 10:46:12 -0700

    creating batch subdir and adding node client example

---
+++ clients/batch/node.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var barrister = require('barrister');
+
+var client = barrister.httpClient("http://localhost:3000/todos");
+
+client.loadContract(function(err) {
+  var batch = client.startBatch();
+  var batchTodoManager = batch.proxy("TodoManager");
+
+  batchTodoManager.createTodo({ 'title' : 'Call Mom', 'completed' : false })
+  batchTodoManager.createTodo({ 'title' : 'Call Dad', 'completed' : false })
+  batchTodoManager.createTodo({ 'title' : 'Wash car', 'completed' : false })
+  batchTodoManager.createTodo({ 'title' : 'Eat Ham', 'completed' : false })
+
+  batch.send(function(err, result) {
+    for (var i = 0, len = result.length; i < len; i++) {
+      // either result[i].error or result[i].result will be set
+      if (result[i].error) {
+        console.log("err.code=" + result[i].error.code);
+      }
+      else {
+        // result from a successful call
+        console.log(result[i].result);
+      }
+    }
+  });
+});
