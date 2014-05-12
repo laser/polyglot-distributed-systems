70f9a98bc56d923158d3ab4731a4a803d5beb412
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-05-12 13:15:19 -0700

    move store impl into TodoManager

--- servers/golang/server.go
+++ servers/golang/server.go
@@ -1,37 +1,57 @@
 package main
 
 import (
-  "fmt"
-  "github.com/coopernurse/barrister-go"
-  "net/http"
-  t "./todos"
-  s "./store"
+	t "./todos"
+	"fmt"
+	"github.com/coopernurse/barrister-go"
+	"net/http"
 )
 
 type TodoManagerImpl struct {
-	Store *s.Store
+	todos   map[int64]t.Todo
+	next_id int64
 }
 
 func (impl TodoManagerImpl) ReadTodos() ([]t.Todo, error) {
-	return impl.Store.GetAll()
+	v := make([]t.Todo, 0, len(impl.todos))
+
+	for _, value := range impl.todos {
+		v = append(v, value)
+	}
+
+	return v, nil
 }
 
 func (impl TodoManagerImpl) CreateTodo(properties t.TodoProperties) (t.Todo, error) {
-	return impl.Store.Save(properties)
+	todo := t.Todo{properties, 1}
+	impl.todos[todo.Id] = todo
+
+	return todo, nil
 }
 
 func (impl TodoManagerImpl) UpdateTodo(todo t.Todo) (t.Todo, error) {
-	return impl.Store.Update(todo)
+	for cached_id := range impl.todos {
+		if todo.Id == cached_id {
+			impl.todos[cached_id] = todo
+		}
+	}
+
+	return todo, nil
 }
 
 func (impl TodoManagerImpl) DeleteTodo(todo t.Todo) (bool, error) {
-	return impl.Store.Delete(todo.Id)
+	for cached_id := range impl.todos {
+		if todo.Id == cached_id {
+			delete(impl.todos, todo.Id)
+		}
+	}
+
+	return true, nil
 }
 
 func main() {
 	idl := barrister.MustParseIdlJson([]byte(t.IdlJsonRaw))
-	store := s.NewStore()
-	mgr := TodoManagerImpl{store}
+	mgr := TodoManagerImpl{make(map[int64]t.Todo), 0}
 	svr := t.NewJSONServer(idl, true, mgr)
 
 	http.Handle("/v1/todos", &svr)
